51a33cf9c3ce3f87792601c4cac162adfd842dbe
Heather Young <hjy.earth@gmail.com>
2020-06-24 13:22:38 -0600

    Added models for Activity, Application, Company, Contact, and Job

---
+++ Models/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorkNotes.Models
+{
+	public enum Status
+	{ 
+		Applied, OfferReceived, Rejected
+	}
+	public class Application
+	{
+		public int ID { get; set; }
+		public int JobID { get; set; }
+		public DateTime Date { get; set; }
+		public Status Status { get; set; }
+
+		public virtual Job Job { get; set; }
+	}
+}
